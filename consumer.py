2de441ebec3a52497f57574472788722c6e2965d
TomSinsel <sinseltom@gmail.com>
2025-08-20 15:23:44 +0200

    Mulitple messages in a single queue

--- consumer.py
+++ consumer.py
@@ -66,7 +66,7 @@ class Consumer:
                                    on_message_callback=lambda ch, method, properties, body: callback(ch, method,
                                                                                                      properties, body,
                                                                                                      self.executor),
-                                   auto_ack=False)
+                                   auto_ack=True)
         try:
             self.channel.start_consuming()
             time.sleep(10)
